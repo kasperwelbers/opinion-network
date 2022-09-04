6708905bc324c48f0b311a29826a90210446f985
Kasper Welbers <kasperwelbers@gmail.com>
2022-09-04 18:06:48 +0200

    fixing local install stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,14 @@ import React from 'react'
 import './Footer.css'
 
 export default ({ globalSettings, socialSettings, navLinks }) => (
-  <footer className='Footer'>
-    <div className='container taCenter'>
-      <span>© 2017 All rights reserved.</span>
+  <footer className="Footer">
+    <div className="container taCenter">
+      <span>
+        This page was created using the{'  '}
+        <a href="https://github.com/Jinksi/netlify-cms-react-starter">
+          Netlify CMS React Starter
+        </a>
+      </span>
     </div>
   </footer>
 )
