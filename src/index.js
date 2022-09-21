fb8926874327387a73407d81071a0ccd2f538754
Kasper Welbers <kasperwelbers@gmail.com>
2022-09-21 07:56:25 +0200

    cleaned out

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import React from 'react'
 //import { render } from 'react-snapshot'
-import { render } from 'react-dom'
+//import { render } from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import 'modern-normalize/modern-normalize.css'
 import './globalStyles.css'
 import App from './App'
-import registerServiceWorker, { unregister } from './registerServiceWorker'
-import data from './data.json'
+// import registerServiceWorker, { unregister } from './registerServiceWorker'
+// import data from './data.json'
 
 const rootEl = document.getElementById('root')
 const root = createRoot(rootEl)
 root.render(<App />)
 
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default
-    render(<NextApp />, rootEl)
-  })
-}
+// if (module.hot) {
+//   module.hot.accept('./App', () => {
+//     const NextApp = require('./App').default
+//     render(<NextApp />, rootEl)
+//   })
+// }
 
 if (process.env.REACT_APP_SITE_URL && 'localStorage' in window) {
   window.localStorage.setItem('netlifySiteURL', process.env.REACT_APP_SITE_URL)
 }
 
-const globalSettings =
-  data.settings && data.settings.filter((doc) => doc.name === 'global')[0]
+// const globalSettings =
+//   data.settings && data.settings.filter((doc) => doc.name === 'global')[0]
 
-if (globalSettings) {
-  globalSettings.enableServiceWorker ? registerServiceWorker() : unregister()
-}
+// if (globalSettings) {
+//   globalSettings.enableServiceWorker ? registerServiceWorker() : unregister()
+// }
